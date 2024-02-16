4288858453cc39320e92d62addc044e8af2fdb00
James Paliga <jamespaliga@hotmail.com>
2024-02-16 20:50:23 +0000

    Create beer model

--- apicall.py
+++ apicall.py
@@ -1,4 +1,5 @@
 import requests, json
+from datetime import datetime
 
 API_URL = "https://api.punkapi.com/v2/beers"
 API_PROPERTIES = ["ibu", "target_fg", "target_og", "ebc",
@@ -12,6 +13,8 @@ beers = resp.json()
 for beer in beers:
     for prop in API_PROPERTIES:
         del beer[prop]
+
+    beer["first_brewed"] = datetime.strptime(beer["first_brewed"], "%m/%Y")
 
 writeFile = open('beers.json', 'w')
 json.dump(beers, writeFile)
--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Beer(models.Model):
+    """
+    Stores information on a single beer
+    """
+    name = models.CharField(max_length=200, unique=True)
+    tagline = models.CharField(max_length=200, unique=True)
+    first_brewed = models.DateField()
+    description = models.TextField()
+    image_url = models.URLField()
+    abv = models.IntegerField()
+    food_pairing = models.JSONField()
