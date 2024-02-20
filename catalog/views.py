ea84876f40d4a3ee7e2041249fcc8fda5f1511a1
James Paliga <jamespaliga@hotmail.com>
2024-02-20 20:07:55 +0000

    Add base template and update index.html to apply Bootstrap stylings

--- catalog/views.py
+++ catalog/views.py
@@ -5,5 +5,4 @@ from .models import Beer
 # Create your views here.
 class BeerList(generic.ListView):
     queryset = Beer.objects.all()
-    # template_name = "catalog/beer_list.html"
     template_name = "catalog/index.html"
