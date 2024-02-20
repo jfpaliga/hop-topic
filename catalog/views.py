c0bccbdebf3d4268b95774e81c3e72bdc2cef5e9
James Paliga <jamespaliga@hotmail.com>
2024-02-20 17:51:49 +0000

    Add Bootstrap template and create staticfiles for deployment

--- catalog/views.py
+++ catalog/views.py
@@ -5,4 +5,5 @@ from .models import Beer
 # Create your views here.
 class BeerList(generic.ListView):
     queryset = Beer.objects.all()
-    template_name = "beer_list.html"
+    # template_name = "catalog/beer_list.html"
+    template_name = "catalog/index.html"
