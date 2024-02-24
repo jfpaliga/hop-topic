8f9d18f04c77012f71ff2cb3edf30de4b48b0743
James Paliga <jamespaliga@hotmail.com>
2024-02-24 17:05:09 +0000

    Add beer model to admin to allow full CRUD functionality for superuser

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+from .models import Beer
 
-# Register your models here.
+
+@admin.register(Beer)
+class BeerAdmin(SummernoteModelAdmin):
+
+    list_display = ('name', 'tagline', 'first_brewed',)
+    search_fields = ['name', 'abv']
+    summernote_fields = ('description',)
--- catalog/models.py
+++ catalog/models.py
@@ -7,8 +7,11 @@ class Beer(models.Model):
     """
     name = models.CharField(max_length=200, unique=True)
     tagline = models.CharField(max_length=200, unique=True)
-    first_brewed = models.CharField()
+    first_brewed = models.CharField(max_length=100)
     description = models.TextField()
     image_url = models.URLField()
     abv = models.IntegerField()
     food_pairing = models.JSONField()
+
+    def __str__(self):
+        return f"{self.name}"
