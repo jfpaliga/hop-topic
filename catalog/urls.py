da0542bb4e619747b353e7ea8a7f9e234645fd69
James Paliga <jamespaliga@hotmail.com>
2024-02-25 13:47:49 +0000

    Add detailed beer view for a user to view more information on a beer

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.BeerList.as_view(), name="home"),
+    path('<int:id>/', views.beer_detail, name="beer_detail"),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Beer
 
@@ -7,3 +7,24 @@ class BeerList(generic.ListView):
     queryset = Beer.objects.all()
     template_name = "catalog/index.html"
     paginate_by = 8
+
+def beer_detail(request, id):
+    """
+    Display an invidivual :model:`catalog.Beer`.
+
+    **Context**
+    ``beer``
+        An instance of :model:`catalog.Beer`.
+
+    ** Template**
+    :template:`blog/post_detail.html`
+    """
+
+    queryset = Beer.objects.all()
+    beer = get_object_or_404(queryset, id=id)
+
+    return render(
+        request,
+        "catalog/beer_detail.html",
+        {"beer": beer,},
+    )
