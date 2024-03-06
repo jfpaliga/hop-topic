6b2b081c163bf539c4eb5128b329f31c2d81beeb
James Paliga <jamespaliga@hotmail.com>
2024-03-06 19:23:37 +0000

    Modify Review model - rating field type changed to integer field with choices 1-5 and alter form widget to radio buttons

--- catalog/forms.py
+++ catalog/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from .models import Review
 
 
+CHOICES = {"1": 1, "2": 2, "3": 3, "4": 4, "5": 5,}
+
 class ReviewForm(forms.ModelForm):
     """
     Form class for users to leave a review on a beer 
@@ -12,3 +14,6 @@ class ReviewForm(forms.ModelForm):
         """
         model = Review
         fields = ('rating', 'body',)
+        widgets ={
+            'rating': forms.RadioSelect(choices=CHOICES),
+        }
--- catalog/models.py
+++ catalog/models.py
@@ -23,11 +23,13 @@ class Review(models.Model):
     Stores a single review on a beer related to :model:`auth.User`
     and :model:`catalog.Beer`.
     """
+    RATINGS = ((1, "1"), (2, "2"), (3, "3"), (4, "4"), (5, "5"),)
+
     beer = models.ForeignKey(
         Beer, on_delete=models.CASCADE, related_name="reviews")
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="reviewer")
-    rating = models.DecimalField(max_digits=2, decimal_places=1)
+    rating = models.IntegerField(default=1, choices=RATINGS)
     body = models.TextField()
     is_approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
