a6a5c0595515eb9c6c4db0df464f3a8db44e33dc
James Paliga <jamespaliga@hotmail.com>
2024-03-13 15:23:43 +0000

    Add page to view all reviews, needs styling

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     path('', views.BeerList.as_view(), name='home'),
     path('filter/<filter_type>/<filter_set>', views.BeerFilterList.as_view(), name='filter'),
     path('<int:id>/', views.beer_detail, name='beer_detail'),
-    path('beeroftheday', views.beer_of_the_day, name='beer_of_the_day'),
     path('<int:id>/delete_review/<int:review_id>', views.delete_review, name='delete_review'),
     path('<int:id>/edit_review/<int:review_id>', views.edit_review, name='edit_review'),
+    path('allreviews', views.all_reviews, name='all_reviews'),
+    path('beeroftheday', views.beer_of_the_day, name='beer_of_the_day'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -149,3 +149,27 @@ def delete_review(request, id, review_id):
         messages.add_message(request, messages.ERROR, 'You can only delete your own reviews!')
 
     return HttpResponseRedirect(reverse('beer_detail', args=[id]))
+
+
+def all_reviews(request):
+    """
+    Display a list of all approved reviews in :model:`catalog.Review`.
+
+    **Context**
+    ``review``
+        An instance of :model:`catalog.Review`.
+
+    ** Template**
+    :template:`catalog/all_reviews.html`
+    """
+    
+    queryset = Review.objects.filter(is_approved=True).order_by("-created_on")
+    context = {
+        "review_list": queryset,
+    }
+
+    return render(
+        request,
+        "catalog/all_reviews.html",
+        context
+    )
