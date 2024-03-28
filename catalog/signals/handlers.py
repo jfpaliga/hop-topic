7dfa488bdb7df9872b80c34230cf86f92a928fa8
James Paliga <jamespaliga@hotmail.com>
2024-03-28 20:28:41 +0000

    Add handler to update average beer rating on deleting a review

--- catalog/signals/handlers.py
+++ catalog/signals/handlers.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
 from users.models import Review
@@ -6,6 +6,13 @@ from catalog.utils import get_beer_average_rating
 
 @receiver(post_save, sender=Review)
 def save_review(sender, instance, created, **kwargs):
+    if instance.is_approved:
+        beer = instance.beer
+        beer.avg_rating = get_beer_average_rating(beer)
+        beer.save()
+
+@receiver(post_delete, sender=Review)
+def del_review(sender, instance, **kwargs):
     if instance.is_approved:
         beer = instance.beer
         beer.avg_rating = get_beer_average_rating(beer)
